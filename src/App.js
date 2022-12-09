b115f913ba1e50e46031eff0610cc363652fadf0
rahul-mehul <rkRahul051@gmail.com>
2022-12-10 00:55:48 +0530

    code commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ToDoLists from "./ToDoLists";
 
-function App() {
+
+const App = () => {
+
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value)
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList]
+    })
+    setInputList("");
+  };
+
+  const deleteItem = (id) => {
+    console.log("delete")
+    setItems((oldItems) => {
+      return oldItems.filter((arrEle, index) => {
+        return index !== id;
+      })
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-div">
+        <div className="center-div">
+
+          <h1> ToDo List </h1>
+
+          <input type="text" placeholder="Add a Items" onChange={itemEvent} value={inputList} />
+
+          <button onClick={listOfItems}>+</button>
+
+          <ol>
+            {Items.map((itemval, index) => {
+              return <ToDoLists key={index}
+                id={index}
+                text={itemval}
+                onSelect={deleteItem}
+              />
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+
   );
 }
 
---
+++ src/ToDoLists.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ToDoLists = (props) => {
+
+
+    return (
+        <>
+
+            <li>
+                <span onClick={() => { props.onSelect(props.id) }}>❌</span>
+                {props.text}
+            </li>
+        </>
+    )
+};
+export default ToDoLists;
